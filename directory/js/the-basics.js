b26b5cdfe2718f0dd3aa5091d8ced0f26c9f9dc0
Oluwalayomi Akinrinade <layomia@gmail.com>
2016-04-15 13:33:25 -0400

    Improved aesthetic of directory page.

--- directory/js/the-basics.js
+++ directory/js/the-basics.js
@@ -61,28 +61,22 @@ $('#multiple-datasets .typeahead').typeahead({
 {
   name: 'users',
   source: users,
+  display: 'name',
   templates: {
     header: '<h3 class="league-name">People</h3>',
     suggestion: function (data) {
-        return '<a href="' + data.link + '"><p><strong>' + data.name + '</strong>, ' +  data.school  + '</p></a>';
+        return '<p><a href="' + data.link + '"><strong>' + data.name + '</strong>, ' +  data.school  + '</a></p>';
     }
   }
 },
 {
   name: 'schools',
   source: schools,
+  display:'school',
   templates: {
     header: '<h3 class="league-name">Schools</h3>',
     suggestion: function (data) {
-        return '<a href="' + data.link + '"><p><strong>' + data.school + '</strong></p></a>';
+        return '<p><a href="' + data.link + '"><strong>' + data.school + '</strong></a></p>';
     }
-  }
-});
-
-$(document).ready(function(){
-  window.onunload=pageleave;
-
-  function pageleave() {
-    $('#multiple-datasets').val = "";
   }
 });
